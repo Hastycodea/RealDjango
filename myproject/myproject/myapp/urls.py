440c9ca0c73c1957860b79d171ad5b9648d404e0
Hastycode Andreh <hastycodeandreh@gmail.com>
2025-04-28 00:29:41 +0300

    mvt

---
+++ myproject/myproject/myapp/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('task_list', views.task_list, name='task_list')
+]
---
+++ myproject/myproject/myapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm 
+
+# Create your views here.
+def task_list(request):
+    tasks = Task.objects.all() 
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+
+        if form.is_valid:
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    
+    return render(request, 'task_list.html', {'tasks': tasks, 'form': form})
+
+    
+
