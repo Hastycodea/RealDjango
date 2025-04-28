18d510159e901730b30466aff7989b7f91b9086a
Hastycode Andreh <hastycodeandreh@gmail.com>
2025-04-28 13:36:21 +0300

    xhtml2PDF

---
+++ listings/mylistings/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('my_listings', views.my_listings, name='my_listings'),
+    path('create_pdf', views.render_pdf_view, name='create_pdf'),
+    path('download_pdf', views.download_pdf, name='download_pdf')
+]
+
---
+++ listings/mylistings/views.py
@@ -0,0 +1,61 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+
+# Create your views here.
+def home(request):
+    return HttpResponse("This is the homepage")
+
+def my_listings(request):
+    return render(request, 'my_listings.html')
+
+def render_pdf_view(request):
+    template_path = 'pdfListings.html'
+    context = {}
+
+    # Create a Django response object, and set content type to PDF
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="report.pdf"'
+
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html,
+       dest=response,
+    #    link_callback=link_callback,  # defined above
+    )
+
+    # if error then show some funny view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+
+    return response
+
+def download_pdf(request):
+    template_path = 'pdfListings.html'
+    context = {}
+
+    # Create a Django response object, and set content type to PDF
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html,
+       dest=response,
+    #    link_callback=link_callback,  # defined above
+    )
+
+    # if error then show some funny view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+
+    return response
