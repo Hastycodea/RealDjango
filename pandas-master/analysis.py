973f05a4c27e9934443b62209b42b364290d55f9
Hastycode Andreh <hastycodeandreh@gmail.com>
2025-05-05 10:22:00 +0300

    comment

--- pandas-master/analysis.py
+++ pandas-master/analysis.py
@@ -1,6 +1,14 @@
-import pandas as pd
+# import pandas as pd
 
-poke = pd.read_csv('pokemon_data.csv')
+# poke = pd.read_csv('pokemon_data.csv')
+# # print(poke.head(2))
 
-print(poke.tail(5))
+# poke_xl = pd.read_excel('pokemon_data.xlsx')
+# # print(poke_xl.head(3))
 
+# poke_tab = pd.read_csv('pokemon_data.txt', delimiter='\t')
+# # print(poke_tab)
+
+# # print(poke.columns)
+
+# print(poke[['Legendary', 'Generation', 'Type 1', 'Name']])
